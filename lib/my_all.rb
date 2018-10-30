9d3c895c0c27576ae579cd687be824b08a6e9a21
Justin Beaulieu <beaulieudev@gmail.com>
2018-10-30 00:08:03 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,9 +7,11 @@ def my_all?(collection)
     i = i + 1
     block_return_values << yield(collection[i])
   end
+
   if block_return_values.include?(false)
     false
   else
     true
   end
+  
 end
